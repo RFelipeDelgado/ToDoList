f832456256a7d66e1f1531d9f2dd6cf58f7a71c1
RFelipeDelgado <raulfeliped@hotmail.com>
2023-10-02 00:46:35 -0300

    todoListrdy!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css'
 import FormularioTareas from './components/Form'
@@ -6,17 +6,33 @@ import Header from './components/Header.jsx'
 import TaskList from './components/TaskList';
 
 const App = () => {
-  const [tasks, setTasks] = useState([])
+  //Get tasks from local storage, if there are. If not, returns empty array
+  const getTasksFromLocalStorageXd = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : []
+  const [tasks, setTasks] = useState(getTasksFromLocalStorageXd)
 
-  const [showCompleted, setShowCompleted] = useState(false)
+  //Get current state of showTasks, from local storage
+  const getShowCompletedFromLocalStorageHehe = localStorage.getItem("showCompleted") ? JSON.parse(localStorage.getItem("showCompleted")) : false;
+  const [showCompleted, setShowCompleted] = useState(getShowCompletedFromLocalStorageHehe)
+
+  //Saves tasks in local storage
+  useEffect(() => {
+    console.log("Cambiaron las tasks");
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+  
+  //Saves showTasks current state
+  useEffect(() => {
+    localStorage.setItem("showCompleted", showCompleted.toString())
+  }, [showCompleted])
+  
   return (
     <div className='contenedor'>
-      <Header setShowCompleted={setShowCompleted} showCompleted={showCompleted}/>
-      <FormularioTareas tasks={tasks} setTasks={setTasks}/>
-      <TaskList 
-      tasks={tasks} 
-      setTasks={setTasks}
-      showCompleted={showCompleted}
+      <Header setShowCompleted={setShowCompleted} showCompleted={showCompleted} />
+      <FormularioTareas tasks={tasks} setTasks={setTasks} />
+      <TaskList
+        tasks={tasks}
+        setTasks={setTasks}
+        showCompleted={showCompleted}
       />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ showCompleted, setShowCompleted }) => {
-    const [eye, setEye] = useState(true)
 
-    const eyeHandler = () => {
-        setEye(!eye)
+    const toggleCompleted = () => {
+        setShowCompleted(!showCompleted)
     }
 
     return (
         <header className='header'>
             <h1 className='header__titulo'>Lista de tareas</h1>
             {showCompleted ?
-            <button onClick={eyeHandler} className='header__boton'>
-                No mostrar tareas completadas
-                <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton' />
-            </button>
-            :
-            <button onClick={eyeHandler} className='header__boton'>
-                No mostrar tareas completadas
-                <FontAwesomeIcon icon={faEye} className='header__icono-boton' />
-            </button>
-            
-        }
+                <button
+                    onClick={() => toggleCompleted()}
+                    className='header__boton'
+                >
+                    No mostrar tareas completadas
+                    <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton' />
+                </button>
+                :
+                <button
+                    onClick={() => toggleCompleted()}
+                    className='header__boton'
+                >
+                    Mostrar tareas completadas
+                    <FontAwesomeIcon icon={faEye} className='header__icono-boton' />
+                </button>
+
+            }
         </header>
     );
 }
